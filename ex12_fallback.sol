91c022012ec42ecedb4541708ee6ea82ece89944
Jacques Chevrier <34470570+jacchevr@users.noreply.github.com>
2018-05-23 09:23:26 -0600

    Update ex12_fallback.sol

--- ex12_fallback.sol
+++ ex12_fallback.sol
@@ -1,1 +1,28 @@
-# example12-solidity
+pragma solidity ^0.4.21;
+
+contract FallbackExample {
+    uint myBalance;
+    
+    function payme() public payable {
+        myBalance += msg.value;
+    }
+    
+    function getBalances() public view returns(uint, uint) {
+        return (address(this).balance, myBalance);
+    }
+    
+    function () public payable {
+        //shouldn't modify the state variable, because it costs gas!
+    }
+
+}
+
+contract FallbackExample2 {
+    function () public payable {
+        
+    }
+    
+    function destroyMe(address _sendEtherTo) public {
+        selfdestruct(_sendEtherTo);
+    }
+}
